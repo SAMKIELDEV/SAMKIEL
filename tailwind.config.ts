497afb6915da3d4a5305deeee7bc25f2ca33e187
SAMKIEL <samuelezekiel488@gmail.com>
2026-04-04 17:46:06 +0100

    feat: initialize global CSS with custom theme variables and update Tailwind configuration with safelists

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,17 @@
 import type { Config } from "tailwindcss";
 
-const config: Config = {
+const config: any = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./lib/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/layout.tsx",
+    "./app/page.tsx",
+  ],
+  safelist: [
+    "bg-emerald-500",
+    "bg-amber-400",
+    "bg-[#888888]",
   ],
   theme: {
     extend: {},
