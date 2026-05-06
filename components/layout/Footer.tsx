7288079de9f28a1c0e495d61c0a8f7ff10fff025
SAMKIEL <samuelezekiel488@gmail.com>
2026-05-06 01:29:28 +0100

    feat: create responsive footer component with navigation links and social media icons

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -61,7 +61,7 @@ export const Footer = () => {
           <div className="max-w-xs">
             <Link href="/" className="inline-block">
               <Image
-                src="/assets/samkiel_logo.png"
+                src="/assets/SAMKIEL_LOGO.png"
                 alt="SAMKIEL"
                 width={160}
                 height={40}
